a4806ee62928a704aeae668eb7637e93b23bbf1a
saulbarbero <82236139+saulbarbero@users.noreply.github.com>
2021-09-14 19:13:16 -0600

    Base

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './component/login/login.component';
 import { PrincipalComponent } from './component/principal/principal.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { TablaComponent } from './component/tabla/tabla.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { FooterComponent } from './component/footer/footer.component';
     LoginComponent,
     PrincipalComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule,
